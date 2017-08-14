088d24d209cfa05f35666b1704521bb9fccd9e1c
Alexandru Guzinschi <alex@gentle.ro>
2017-08-14 12:26:59 +0300

    added Cnp

---
+++ src/Exception/InvalidCnpException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright 2017 Alexandru Guzinschi <alex@gentle.ro>
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+namespace Vimishor\Cnp\Exception;
+
+/**
+ * @author Alexandru Guzinschi <alex@gentle.ro>
+ */
+final class InvalidCnpException extends \Exception
+{
+}
